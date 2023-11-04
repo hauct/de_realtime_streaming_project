16e56768a340ef077d705ae33bfb397ee75a4f1e
hauct <cungtronghau@gmail.com>
2023-11-05 01:50:49 +0700

    edit something

--- script/entry.sh
+++ script/entry.sh
@@ -8,7 +8,7 @@ if [ -e "/opt/airflow/requirements.txt" ]; then
     # Check if the file /opt/airflow/requirements.txt exists
     $(command python) pip install --upgrade pip
     # Upgrade pip using the Python interpreter found in the system's PATH
-    $(command -v pip) install --user -r requirements.txt
+    $(command -v pip) install --user -r /opt/airflow/requirements.txt
     # Install Python packages listed in requirements.txt for the user
 fi
 
